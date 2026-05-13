87f57f2fca7c388d433d262eeae5bab122060707
unknown <1724165744@qq.com>
2026-05-13 19:00:16 +0800

    fix: 移除vercel adapter，改回静态构建

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel';
-
 export default defineConfig({
   output: 'static',
-  site: 'https://ajmd.vercel.app',
-  adapter: vercel(),
+  site: 'https://ajmd-site-uamy.vercel.app',
 });
