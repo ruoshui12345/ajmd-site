bb8d7cfbe38e41173bf764a1170f83bd38fcfdfc
unknown <1724165744@qq.com>
2026-05-13 19:11:03 +0800

    fix: vercel.json改为@astrojs/vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   output: 'static',
+  adapter: vercel(),
   site: 'https://ajmd-site-uamy.vercel.app',
 });
