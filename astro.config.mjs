758bb6c7c9ca63899369bfe111accb3c74f2bf5c
unknown <1724165744@qq.com>
2026-05-13 19:15:10 +0800

    fix: 去除所有adapter，恢复最简配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: { enabled: false },
-  }),
   site: 'https://ajmd-site-uamy.vercel.app',
 });
